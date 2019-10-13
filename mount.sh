7cee1496276fa3f0ef6b5f52b2ebfd5a437e3126
Alexander Kuemmel <akisys@alexanderkuemmel.com>
2019-10-14 00:31:52 +0200

    ADD: mount helperscript to allow mounting a restic repository from within the container

---
+++ mount.sh
@@ -0,0 +1,27 @@
+#/bin/sh
+
+PROFILE="${1}"
+HOST_MOUNTPATH="${2}"
+
+if [ -z "${PROFILE}" ]; then
+  echo "need profile"; exit 1;
+fi
+
+if [ -z "${HOST_MOUNTPATH}" ]; then
+  echo "need host mountpath"; exit 1;
+fi
+
+cd "$(dirname $0)"
+echo "sudo permissions required for remounting binds"
+sudo mount --bind -o shared "${HOST_MOUNTPATH}" "${HOST_MOUNTPATH}"
+
+docker-compose run \
+  --rm \
+  -v "${HOST_MOUNTPATH}:${HOST_MOUNTPATH}:ro,shared" \
+  restic-backup \
+  -p "${PROFILE}" \
+  mount "${HOST_MOUNTPATH}"
+
+echo "sudo permissions required for unmounting previous binds"
+sudo umount /mnt/restore
+
